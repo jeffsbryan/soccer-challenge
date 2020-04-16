efe39457aadca6cbf2b48308a2a8dffd8a5d1435
Jeff Bryan <jbryan@isynergetics.com>
2020-04-15 22:03:12 -0400

    Added WOD function

--- src/app/workout/wod-post/wod-post.component.ts
+++ src/app/workout/wod-post/wod-post.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-wod-post',
-  templateUrl: './wod-post.component.html',
-  styleUrls: ['./wod-post.component.css']
+  selector: "app-wod-post",
+  templateUrl: "./wod-post.component.html",
+  styleUrls: ["./wod-post.component.css"],
 })
 export class WodPostComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSaveWodResult(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    //this.workoutService.addWodResult(form.value.result, form.value.comment);
+    form.resetForm();
   }
-
 }
