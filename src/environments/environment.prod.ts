dd826cafae7eb412a7e35fdab9b3f4cbc3efa4a9
Jeff Bryan <jbryan@isynergetics.com>
2020-06-03 14:49:25 -0400

    Added SSL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://ec2-35-175-104-219.compute-1.amazonaws.com:3000/api",
+  apiUrl: "https://ec2-35-175-104-219.compute-1.amazonaws.com/api",
 };
